552b24d88713f9e4e4f951cec60d0742d24c9e3d
Amanda Mark <amarkpark@gmail.com>
2016-10-17 22:13:34 +0000

    Added On and Off. ~AMP

--- src/main.py
+++ src/main.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Purple Unicorns RULE THE WORLD!!!'
 
+@app.route('/on')
+def get_on():
+	return "Warmer ON <a href='/off'>Turn Warmer OFF</a> <a href='/''>Return to Main</a>"
+
+@app.route('/off')
+	return "Warmer OFF <a href='/on'>Turn Warmer ON</a> <a href='/''>Return to Main</a>"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
