bc980780881e4a8b28f7d53aa7d06389cce8179f
Amanda Mark <amarkpark@gmail.com>
2016-10-17 23:11:35 +0000

    Removed all links for now. ~AMP

--- src/main.py
+++ src/main.py
@@ -3,16 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	print("Purple Unicorns RULE THE WORLD!!! \n")
-    return "<a href='/on'>Turn Warmer ON</a> or <a href='/off'>Turn Warmer OFF</a>"
+    return "Purple Unicorns RULE THE WORLD!!!"
 
 @app.route('/on')
-def get_on():
-	return "Warmer ON <a href='/off'>Turn Warmer OFF</a> <a href='/''>Return to Main</a>"
+def on():
+	return "Warmer ON"
 
 @app.route('/off')
-def get_off():
-	return "Warmer OFF <a href='/on'>Turn Warmer ON</a> <a href='/''>Return to Main</a>"
+def off():
+	return "Warmer OFF"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
