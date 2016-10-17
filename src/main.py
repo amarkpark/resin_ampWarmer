e891f262708d09839f5cf752058e7d5ec3b94d55
Amanda Mark <amarkpark@gmail.com>
2016-10-17 21:27:58 +0000

    needs to be port 80 ~AMP

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Purple Unicorns RULE THE WORLD!!!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8090)
+    app.run(host='0.0.0.0', port=80)
